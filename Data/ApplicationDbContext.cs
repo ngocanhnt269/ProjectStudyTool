8c20a5e35bd818c0b8045b17b3fe1a8547a76d79
ngocanhnt269 <ngocanhnt269@gmail.com>
2024-04-01 14:41:34 -0700

    Added data

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,4 +11,15 @@ public class ApplicationDbContext : IdentityDbContext
     {
     }
     public DbSet<User>? User { get; set; } = default!;
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<User>().Property(u => u.UserId).IsRequired();
+        modelBuilder.Entity<User>().ToTable("User");
+        // modelBuilder.Seed();
+    }
+
+    public DbSet<ProjectStudyTool.Models.Card> Card { get; set; } = default!;
+
+    public DbSet<ProjectStudyTool.Models.CardSet> CardSet { get; set; } = default!;
 }
