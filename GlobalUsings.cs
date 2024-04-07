9256021dacb404e0f8ee82b0203e46e1d74a9c66
ngocanhnt269 <ngocanhnt269@gmail.com>
2024-04-07 10:31:25 -0700

    Reset main branch

--- GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using System.ComponentModel.DataAnnotations;
-global using ProjectStudyTool.Models;
-global using ProjectStudyTool.Controllers;
-global using ProjectStudyTool.Data;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-global using System.Configuration;
--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ProjectStudyTool.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
